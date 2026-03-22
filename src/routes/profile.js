51b7c842132a6372e0c49aef60602a5dc5ff8e4d
shubhika20 <shubhika.agrawal98@gmail.com>
2026-03-22 15:27:46 +0530

    Added api to edit user profile

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,14 +1,35 @@
 const express = require("express");
 const userAuth = require("../middleware/auth");
+const { validateProfileEditData } = require("../utils/validation");
 
 const profileRouter = express.Router();
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
   } catch (error) {
     res.status(400).send("ERROR " + error.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateProfileEditData(req)) {
+      throw new Error("Invalid edit request");
+    }
+    const loggedInUser = req.user;
+    Object.keys(req.body).forEach((key) => {
+      loggedInUser[key] = req.body[key];
+    });
+    await loggedInUser.save();
+    //res.send(`${loggedInUser.firstName}, your profile is updated successfully`); one way to send response
+    res.json({
+      message: `${loggedInUser.firstName}, your profile is updated successfully`,
+      data: loggedInUser,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR " + err.message);
   }
 });
 
